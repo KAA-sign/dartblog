9aab855c8834777df1ddda9d1d19b440e34174cb
KAA-sign <q89308033587@gmail.com>
2021-02-01 20:15:40 +0300

    +

--- siteblog/blog/templatetags/menu.py
+++ siteblog/blog/templatetags/menu.py
@@ -4,6 +4,7 @@ from blog.models import Category
 
 register = template.Library()
 
+
 @register.inclusion_tag('blog/menu_tpl.html')
 def show_menu(menu_class='menu'):
     categories = Category.objects.all()
